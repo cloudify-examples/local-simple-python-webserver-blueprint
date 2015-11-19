3b1f6b3aa02e01f767cd7c44754758bb04c52162
nir0s <nir36g@gmail.com>
2015-11-19 22:24:58 +0200

    mac on the way

--- deploy.py
+++ deploy.py
@@ -6,24 +6,33 @@ from cloudify import ctx
 
 
 IS_WIN = os.name == 'nt'
+# is this needed?
+IS_DARWIN = (sys.platform == 'darwin')
+PORT = ctx.node.properties['port']
 
-port = ctx.node.properties['port']
 
-webserver_cmd = [sys.executable, '-m', 'SimpleHTTPServer', str(port)]
-if not IS_WIN:
-    webserver_cmd.insert(0, 'nohup')
+def run_server():
+    webserver_cmd = [sys.executable, '-m', 'SimpleHTTPServer', str(PORT)]
+    if not IS_WIN:
+        webserver_cmd.insert(0, 'nohup')
 
-ctx.logger.info('Running WebServer locally on port: {0}'.format(port))
-DN = open(os.devnull, 'wb')
-process = subprocess.Popen(webserver_cmd, shell=False, stdout=DN, stderr=DN)
+    ctx.logger.info('Running WebServer locally on port: {0}'.format(PORT))
+    dn = open(os.devnull, 'wb')
+    process = subprocess.Popen(webserver_cmd, stdout=dn, stderr=dn)
 
-if not IS_WIN:
-    del webserver_cmd[0]
-    get_pid = ['pidof', '-s']
-    get_pid.extend(webserver_cmd)
-    pid = int(subprocess.check_output(get_pid))
-else:
-    pid = process.pid
+    if not IS_WIN:
+        del webserver_cmd[0]
+        get_pid = ['pidof', '-s']
+        get_pid.extend(webserver_cmd)
+        return int(subprocess.check_output(get_pid))
+    else:
+        return process.pid
 
-ctx.logger.info('Setting `pid` runtime property: {0}'.format(pid))
-ctx.instance.runtime_properties['process'] = process
+
+def set_pid(pid):
+    ctx.logger.info('Setting `pid` runtime property: {0}'.format(pid))
+    ctx.instance.runtime_properties['pid'] = pid
+
+
+pid = run_server()
+set_pid(pid)
