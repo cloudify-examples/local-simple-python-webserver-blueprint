0a1cf96f1b33d9d08470e3f883415415adc618ba
nir0s <nir36g@gmail.com>
2015-11-19 22:14:34 +0200

    windows!

--- deploy.py
+++ deploy.py
@@ -13,23 +13,17 @@ webserver_cmd = [sys.executable, '-m', 'SimpleHTTPServer', str(port)]
 if not IS_WIN:
     webserver_cmd.insert(0, 'nohup')
 
+ctx.logger.info('Running WebServer locally on port: {0}'.format(port))
 DN = open(os.devnull, 'wb')
-ctx.logger.info('Running WebServer on port: {0}'.format(port))
-
-ctx.logger.info('Executing: {0}'.format(webserver_cmd))
-# process = subprocess.Popen(
-#     webserver_cmd, shell=False, stdout=DN, stderr=DN, preexec_fn=os.setsid)
-process = subprocess.Popen(
-    webserver_cmd, shell=False, stdout=DN, stderr=DN)
-# pid = str(process.pid)
-# ctx.logger.info('PID: {0}'.format(pid))
+process = subprocess.Popen(webserver_cmd, shell=False, stdout=DN, stderr=DN)
 
 if not IS_WIN:
     del webserver_cmd[0]
     get_pid = ['pidof', '-s']
     get_pid.extend(webserver_cmd)
     pid = int(subprocess.check_output(get_pid))
+else:
+    pid = process.pid
 
-# ctx.logger.info(os.system('ps aux | grep SimpleHTTPServer'))
 ctx.logger.info('Setting `pid` runtime property: {0}'.format(pid))
 ctx.instance.runtime_properties['process'] = process
