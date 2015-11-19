50a39201a1285be1f725450820766aa967cac8ea
nir0s <nir36g@gmail.com>
2015-11-19 21:29:51 +0200

    x

--- deploy.py
+++ deploy.py
@@ -15,7 +15,14 @@ if not IS_WIN:
 
 DN = open(os.devnull, 'wb')
 ctx.logger.info('Running WebServer on port: {0}'.format(port))
-process = subprocess.Popen(webserver_cmd, shell=False, stdout=DN, stderr=DN)
+
+ctx.logger.info('Executing: {0}'.format(webserver_cmd))
+# process = subprocess.Popen(
+#     webserver_cmd, shell=False, stdout=DN, stderr=DN, preexec_fn=os.setsid)
+process = subprocess.Popen(
+    webserver_cmd, shell=False, stdout=DN, stderr=DN)
+# pid = str(process.pid)
+# ctx.logger.info('PID: {0}'.format(pid))
 
 if not IS_WIN:
     del webserver_cmd[0]
@@ -23,5 +30,6 @@ if not IS_WIN:
     get_pid.extend(webserver_cmd)
     pid = int(subprocess.check_output(get_pid))
 
+# ctx.logger.info(os.system('ps aux | grep SimpleHTTPServer'))
 ctx.logger.info('Setting `pid` runtime property: {0}'.format(pid))
-ctx.instance.runtime_properties['pid'] = pid
+ctx.instance.runtime_properties['process'] = process
--- uninstall.py
+++ uninstall.py
@@ -4,6 +4,7 @@ import signal
 from cloudify import ctx
 
 pid = ctx.instance.runtime_properties['pid']
+ctx.logger.info('Running process PID: {0}'.format(pid))
 try:
     os.kill(pid, signal.SIGTERM)
     ctx.logger.info('Python Webserver Terminated!')
