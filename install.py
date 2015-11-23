7e9fcb753e862418e2168491948987c8a1885d1c
nir0s <nir36g@gmail.com>
2015-11-23 21:46:30 +0200

    optimizations

--- install.py
+++ install.py
@@ -11,10 +11,9 @@ from cloudify import ctx
 
 
 IS_WIN = os.name == 'nt'
-IS_DARWIN = (sys.platform == 'darwin')
 
-# Get the port from the blueprint. We're running in the context of the
-# `http_web_server` node so we can read its `port` property.
+# Get the port from the blueprint. We're running this script in the context of
+# the `http_web_server` node so we can read its `port` property.
 PORT = ctx.node.properties['port']
 
 
@@ -26,8 +25,8 @@ def run_server():
     # The ctx object provides a built in logger.
     ctx.logger.info('Running WebServer locally on port: {0}'.format(PORT))
     # emulating /dev/null
-    dn = open(os.devnull, 'wb')
-    process = subprocess.Popen(webserver_cmd, stdout=dn, stderr=dn)
+    with open(os.devnull, 'wb') as dn:
+        process = subprocess.Popen(webserver_cmd, stdout=dn, stderr=dn)
     return process.pid
 
 
