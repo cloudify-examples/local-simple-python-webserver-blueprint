b461bc76162a03604b4306f026fb32c05949c604
JPHess <jeremy@cloudify.co>
2018-08-06 13:54:27 +0300

    correct path

--- install.py
+++ install.py
@@ -18,6 +18,7 @@ PORT = ctx.node.properties['port']
 
 
 def run_server():
+    os.chdir('/opt/manager/resources/blueprints/default_tenant/local-simple-python-webserver-blueprint')
     webserver_cmd = [sys.executable, '-m', 'SimpleHTTPServer', str(PORT)]
     if not IS_WIN:
         webserver_cmd.insert(0, 'nohup')
